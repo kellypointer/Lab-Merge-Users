0f91af170247e3eb31c9aa7289cd03db2c281c2f
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-03-12 20:58:29 -0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
+const users1 = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Henry', 'Ivy', 'Jack'];
+const users2 = ["Jim3","Pam5","Dwight77"];
 
+const users = [...users1, ...users2]
+
+function combineUsers() {
+  const combinedObject = { 
+    users: []
+  }
+}
+  let today = new Date().toString("M/d/yyyy");
+
+  combinedObject.merge_date = today;
+
+  return combinedObject;
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
